6d5485eb3b697d75bf632e123efa5235dbb250fb
Damian <67106546+damger9@users.noreply.github.com>
2026-09-08 20:29:33 +0200

    Add fix for issues

--- src/db/Client.ts
+++ src/db/Client.ts
@@ -3,4 +3,4 @@ import { PrismaClient } from '../prisma/client';
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
-export const Client = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter });
