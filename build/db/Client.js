e989192e69ea6ca2f8d2ae9ee31235853cf4291a
Damian <67106546+damger9@users.noreply.github.com>
2026-09-08 21:21:46 +0200

    Fix previous commit (#15)

---
+++ build/db/Client.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.prisma = void 0;
+const adapter_pg_1 = require("@prisma/adapter-pg");
+const client_1 = require("../prisma/client");
+const adapter = new adapter_pg_1.PrismaPg({ connectionString: process.env.DATABASE_URL });
+exports.prisma = new client_1.PrismaClient({ adapter });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ2xpZW50LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2RiL0NsaWVudC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7QUFBQSxtREFBOEM7QUFDOUMsNkNBQWdEO0FBRWhELE1BQU0sT0FBTyxHQUFHLElBQUkscUJBQVEsQ0FBQyxFQUFFLGdCQUFnQixFQUFFLE9BQU8sQ0FBQyxHQUFHLENBQUMsWUFBWSxFQUFFLENBQUMsQ0FBQztBQUVoRSxRQUFBLE1BQU0sR0FBRyxJQUFJLHFCQUFZLENBQUMsRUFBRSxPQUFPLEVBQUUsQ0FBQyxDQUFDIn0=
